ea725dcbf88f2e9eebb8a76bdc3eeb95e300534b
Naoki Hara <naoki.hara@dts-insight.co.jp>
2026-03-17 17:45:10 +0900

    apply weights_only=True to torch.load()

--- yolox/core/trainer.py
+++ yolox/core/trainer.py
@@ -271,7 +271,7 @@ class Trainer:
             else:
                 ckpt_file = self.args.ckpt
 
-            ckpt = torch.load(ckpt_file, map_location=self.device)
+            ckpt = torch.load(ckpt_file, map_location=self.device, weights_only=True)
             # resume the model/optimizer state dict
             model.load_state_dict(ckpt["model"])
             self.optimizer.load_state_dict(ckpt["optimizer"])
@@ -290,7 +290,7 @@ class Trainer:
             if self.args.ckpt is not None:
                 logger.info("loading checkpoint for fine tuning")
                 ckpt_file = self.args.ckpt
-                ckpt = torch.load(ckpt_file, map_location=self.device)["model"]
+                ckpt = torch.load(ckpt_file, map_location=self.device, weights_only=True)["model"]
                 model = load_ckpt(model, ckpt)
             self.start_epoch = 0
 
